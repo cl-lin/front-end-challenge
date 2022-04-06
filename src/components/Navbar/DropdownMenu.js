76dd1d53f07a010e90c253c0edec5c307b487f4f
cl-lin <clin3105@gmail.com>
2022-04-06 12:13:31 +1200

    fix alignment on right side navbar

--- src/components/Navbar/DropdownMenu.js
+++ src/components/Navbar/DropdownMenu.js
@@ -1,7 +1,12 @@
+// import arrow from '../../images/icon-arrow.svg'
+import { ReactComponent as Arrow } from '../../images/icon-arrow.svg'
 
 export default function DropdownMenu () {
     console.log('dropdown')
         return (
-    <a className="nav-link" href="/">teacher@school.org</a>
+            <>
+    <a className="nav-link email-add" href="/">teacher@school.org<Arrow className="arrow"/></a>
+    {/* <a className="arrow"><Arrow /></a> */}
+            </>
         )
     }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,53 +3,21 @@ import DropdownMenu from "./DropdownMenu"
 import NavbarMenu from "./NavbarMenu"
 import './Navbar.css'
 import logo from '../../images/logo.svg'
-import arrow from '../../images/icon-arrow.svg'
+// import arrow from '../../images/icon-arrow.svg'
 
 export default function Navbar () {
 console.log('navbar')
     return (
         <nav className="navbar nav-flex">
 
-                 <img className="" src={logo} alt="logo"/>
+                 <img className="logo-img" src={logo} alt="logo"/>
             <div>
                  <NavbarMenu /> 
             </div>
 
-            <div>
+            <div className="dropdown-position">
                 <DropdownMenu />
-                <img src={arrow} alt="arrowdown"/>
             </div>
         </nav>
-
-        // <nav className ="navbar">
-        //     <section className ="flex-container">
-
-        //     <div className="logo">
-        //         <img className="logo-img" src={logo} alt="logo"/>
-        //     </div>
-
-        //     <div className="nav-link-flex">
-        //     <ul>
-        //         {NavbarMenu.map((menu, index) => {
-        //             return (
-        //                 <li key ={index}>
-        //                     <a className={menu.className} href={menu.url}>
-        //                         {menu.title}
-        //                     </a>
-        //                 </li>
-        //             )
-        //         }) }
-        //     </ul>
-        //     </div> 
-
-        //     <div className="user-flex">
-        //         <a href="/">
-        //                  <DropdownMenu />
-        //                  <img src={arrow} alt="arrowdown"/>
-        //         </a>
-        //     </div>
-        //         </section>
-  
-        // </nav>
     )
 }
