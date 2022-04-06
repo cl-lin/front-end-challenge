f7a65545917300de8eb6db3613b39b6aeb308d70
cl-lin <clin3105@gmail.com>
2022-04-06 16:10:36 +1200

    drop down menu

---
+++ src/components/Navbar/DropdownItem.js
@@ -0,0 +1,20 @@
+import './Navbar.css'
+
+export default function DropdownItem() {
+
+    return (
+        <section className="menu menu-box">
+            <div className="menu-item">
+                student@school.org
+            </div>
+            <div className="menu-item">
+                Settings
+            </div>
+            <div className="menu-item">
+                Sign Out
+            </div>
+        </section>
+
+    )
+
+}
--- src/components/Navbar/DropdownMenu.js
+++ src/components/Navbar/DropdownMenu.js
@@ -1,12 +1,19 @@
 // import arrow from '../../images/icon-arrow.svg'
 import { ReactComponent as Arrow } from '../../images/icon-arrow.svg'
+import React, {useState} from 'react';
+import DropdownItem from "./DropdownItem"
 
 export default function DropdownMenu () {
+    const [open, setOpen] = useState(false);
+
     console.log('dropdown')
         return (
             <>
-    <a className="nav-link email-add" href="/">teacher@school.org<Arrow className="arrow"/></a>
-    {/* <a className="arrow"><Arrow /></a> */}
+                <a className="nav-link email-add" onClick={() => setOpen(!open)}>teacher@school.org</a>
+                <Arrow className="arrow"/>
+                {open === true && 
+                <DropdownItem />
+                }
             </>
         )
     }
